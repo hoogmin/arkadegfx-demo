518af6b32c9643a2c311a94bf73be72be50f3587
Javier Martinez <hoogmin@gmail.com>
2024-04-25 22:51:24 -0400

    Start booking page and about page.

---
+++ app/book-us/page.jsx
@@ -0,0 +1,71 @@
+"use client"
+
+import React from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const BookUsPage = () => {
+
+    const bookingSubmitHandler = () => {
+        toast.success("Booking request sent!");
+        const confirmText = document.getElementById("confirmText");
+        confirmText.classList.remove("hidden");
+    }
+
+    return (
+        <div className="container flex flex-col justify-center items-center w-[100vw] h-[100vh] mx-auto bg-[url('/img/bookus_bg.jpg')] px-8 pt-6 pb-8">
+            <div className="mb-4">
+                <label 
+                className="block text-white text-sm font-bold mb-2 drop-shadow-md" 
+                htmlFor="name">
+                    Name
+                </label>
+                <input 
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-black bg-white leading-tight focus:outline-none focus:shadow-outline" 
+                id="name"
+                placeholder="Your Name"
+                type="text" />
+            </div>
+            <div className="mb-4">
+                <label 
+                className="block text-white text-sm font-bold mb-2 drop-shadow-md" 
+                htmlFor="email">
+                    Email
+                </label>
+                <input 
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-black bg-white leading-tight focus:outline-none focus:shadow-outline" 
+                id="email"
+                placeholder="person@example.com"
+                type="email" />
+            </div>
+            <div className="mb-4">
+                <label 
+                className="block text-white text-sm font-bold mb-2 drop-shadow-md" 
+                htmlFor="message">
+                    Booking Message
+                </label>
+                <textarea 
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-black bg-white leading-tight focus:outline-none focus:shadow-outline"
+                placeholder="I would like to book for Saturday at 3:30 PM..."
+                name="message" 
+                id="message" 
+                cols="30" 
+                rows="10">
+                </textarea>
+            </div>
+            <div className="flex flex-col items-center justify-between">
+                <button 
+                className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                onClick={bookingSubmitHandler}>
+                    Submit
+                </button>
+                <p className="hidden text-green-400 text-bold drop-shadow-md bg-gray-500 mt-3 rounded p-2" id="confirmText">
+                    Sent! An Email from us will confirm your booking.
+                </p>
+                <ToastContainer/>
+            </div>
+        </div>
+    )
+}
+
+export default BookUsPage;
--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -4,7 +4,7 @@ const redHatMono = Red_Hat_Mono({ subsets: ["latin"], weight: "400" });
 
 const Footer = () => {
     return (
-        <footer className={`${redHatMono.className} w-full text-center absolute bottom-0 z-10 mt-auto`}>
+        <footer className={`${redHatMono.className} w-full text-center bottom-0 z-10 mt-auto`}>
             &copy; Javier Martinez 2024 - BSD-3-Clause - Demo Website
         </footer>
     )
